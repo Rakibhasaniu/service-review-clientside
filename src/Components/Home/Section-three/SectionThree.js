8fd0d8025fc9e0b17c3e10a2393459f9ffce7535
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-11-10 21:15:15 +0600

    bannner three added

--- src/Components/Home/Section-three/SectionThree.js
+++ src/Components/Home/Section-three/SectionThree.js
@@ -5,23 +5,23 @@
 //         <>
 
 
-//             <section className="py-6 dark:bg-gray-800 dark:text-gray-50 lg:px-20">
-//                 <h1 className='text-center text-4xl pb-4 text-shadows'>Gallary</h1>
-//                 <div className="container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4">
-//                     <img src="https://i.ibb.co/87KdQJ0/photo-1512152272829-e3139592d56f-crop-faces-edges-cs-tinysrgb-fit-crop-fm-jpg-ixid-Mnwx-Mj-A3f-DB8-M.jpg" alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 dark:bg-gray-500 aspect-square" />
-//                     <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://i.ibb.co/CwhxG26/wwj1hrnad01x.jpg" />
-//                     <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://i.ibb.co/TgJf7V4/download.jpg" />
-//                     <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://i.ibb.co/k5hbhry/download-4.jpg" />
-//                     <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://i.ibb.co/bBHX5C9/download-1.jpg" />
-//                     <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://i.ibb.co/87KdQJ0/photo-1512152272829-e3139592d56f-crop-faces-edges-cs-tinysrgb-fit-crop-fm-jpg-ixid-Mnwx-Mj-A3f-DB8-M.jpg" />
-//                     <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://i.ibb.co/TgJf7V4/download.jpg" />
-//                     <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://i.ibb.co/dQVzdfR/download-2.jpg" />
-//                     <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://i.ibb.co/k5hbhry/download-4.jpg" />
-//                     <img src="https://i.ibb.co/TgJf7V4/download.jpg" alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-1 md:row-start-3 dark:bg-gray-500 aspect-square" />
-//                 </div>
-//             </section>
-//         </>
-//     );
-// };
+            <section className="py-6 dark:bg-gray-800 dark:text-gray-50 lg:px-20">
+                <h1 className='text-center text-4xl pb-4 text-shadows'>Gallary</h1>
+                <div className="container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4">
+                    <img src="https://i.ibb.co/87KdQJ0/photo-1512152272829-e3139592d56f-crop-faces-edges-cs-tinysrgb-fit-crop-fm-jpg-ixid-Mnwx-Mj-A3f-DB8-M.jpg" alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 dark:bg-gray-500 aspect-square" />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://i.ibb.co/CwhxG26/wwj1hrnad01x.jpg" />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://i.ibb.co/TgJf7V4/download.jpg" />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://i.ibb.co/k5hbhry/download-4.jpg" />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://i.ibb.co/bBHX5C9/download-1.jpg" />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://i.ibb.co/87KdQJ0/photo-1512152272829-e3139592d56f-crop-faces-edges-cs-tinysrgb-fit-crop-fm-jpg-ixid-Mnwx-Mj-A3f-DB8-M.jpg" />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://i.ibb.co/TgJf7V4/download.jpg" />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://i.ibb.co/dQVzdfR/download-2.jpg" />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://i.ibb.co/k5hbhry/download-4.jpg" />
+                    <img src="https://i.ibb.co/TgJf7V4/download.jpg" alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-1 md:row-start-3 dark:bg-gray-500 aspect-square" />
+                </div>
+            </section>
+        </>
+    );
+};
 
-// export default SectionThree;
+export default SectionThree;
