fa8a138b50aed929d1263290a271825deb3f8426
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-11-10 21:13:56 +0600

    cart created

--- src/Components/Home/Cards.js
+++ src/Components/Home/Cards.js
@@ -17,21 +17,21 @@
 //                 </PhotoProvider>
 //                 {/* ----------------phot zoom ent--------------- */}
 
-//                 <div className="card-body">
-//                     <div className="justify-between flex">
-//                         <h2 className="card-title text-3xl"> {services?.title} </h2>
-//                         <div className="badge badge-secondary ml-2">{services?.rating}</div>
-//                     </div>
-//                     <p>{services?.details.slice(0, 100) + "..."}</p>
-//                     <div className="card-actions justify-between">
-//                         <h1 className="text-1xl text-success"> Price: {services?.price}</h1>
-//                         <Link to={`/card_details/${services?._id}`} > <button           type="button"
-//           className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-sky-400 dark:text-gray-900">Details.</button> </Link>
-//                     </div>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// };
+                <div className="card-body">
+                    <div className="justify-between flex">
+                        <h2 className="card-title text-3xl"> {services?.title} </h2>
+                        <div className="badge badge-secondary ml-2">{services?.rating}</div>
+                    </div>
+                    <p>{services?.details.slice(0, 100) + "..."}</p>
+                    <div className="card-actions justify-between">
+                        <h1 className="text-1xl text-success"> Price: {services?.price}</h1>
+                        <Link to={`/card_details/${services?._id}`} > <button           type="button"
+          className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-sky-400 dark:text-gray-900">Details.</button> </Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
 
-// export default Cards;
+export default Cards;
